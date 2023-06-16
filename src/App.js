dfaa442d36d56e6df24510bbe917b0e01b1d2720
HEMALATHA01492 <hema01492@gmail.com>
2023-06-16 11:55:52 +0530

    task1.1

--- src/App.js
+++ src/App.js
@@ -1,58 +1,91 @@
 
-// function App() {
-
-// let course='Half Stack Application Developement';
-// let part1='Fundamentals of React';
-// let ex1=10;
-// let part2='Using props to pass data';
-// let ex2=7;
-// let part3='State of a compenent';
-// let ex3=14;
-
-//   return (
-//     <div>
-//       <h1>{course}</h1>
-//       <p>{part1} {ex1}</p>
-//       <p>{part2} {ex2}</p>
-//       <p>{part3} {ex3}</p>
-//       <p>Number of Exercises { ex1 + ex2 + ex3 }</p>
-//     </div>
-    
-//   );
-// }
-
-// export default App;
-//-------------------------
-import React from 'react';
-function Header(props){
-  return(
-    <h1>{props.course}</h1>
-  )
-}
-function Content(props){
-  return(
-    <div>
-    <p>{props.part1} {props.ex1}</p>
-    <p>{props.part2} {props.ex2}</p>
-    <p>{props.part3} {props.ex3}</p>
-    </div>
-  )
-}
-function Total(props){
-  return(
-<p>Number of Exercises {props.ex1 + props.ex2 + props.ex3 }</p>  )
-}
-
 function App() {
+
+let course='Half Stack Application Developement';
+let part1='Fundamentals of React';
+let ex1=10;
+let part2='Using props to pass data';
+let ex2=7;
+let part3='State of a compenent';
+let ex3=14;
+
   return (
     <div>
-      <Header course='Half Stack Application Developement'/>
-      <Content part1='Fundamentals of React'   ex1={10}
-              part2='Using props to pass data' ex2={7}
-              part3='State of a compenent'     ex3={14} />
-      <Total ex1={10} ex2={7} ex3={14}/>
+      <h1>{course}</h1>
+      <p>{part1} {ex1}</p>
+      <p>{part2} {ex2}</p>
+      <p>{part3} {ex3}</p>
+      <p>Number of Exercises { ex1 + ex2 + ex3 }</p>
     </div>
-  )
+    
+  );
 }
 
 export default App;
+//-------------------------
+// import React from 'react';
+// function Header(props){
+//   return(
+//     <h1>{props.course}</h1>
+//   )
+// }
+// function Content(props){
+//   return(
+//     <div>
+//     <p>{props.part1} {props.ex1}</p>
+//     <p>{props.part2} {props.ex2}</p>
+//     <p>{props.part3} {props.ex3}</p>
+//     </div>
+//   )
+// }
+// function Total(props){
+//   return(
+// <p>Number of Exercises {props.ex1 + props.ex2 + props.ex3 }</p>  )
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Header course='Half Stack Application Developement'/>
+//       <Content part1='Fundamentals of React'   ex1={10}
+//               part2='Using props to pass data' ex2={7}
+//               part3='State of a compenent'     ex3={14} />
+//       <Total ex1={10} ex2={7} ex3={14}/>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// import React, { useState } from 'react'
+
+// function App() {
+
+//   const [good, setGood]=useState(0);
+//   const [neutral,setNeutral]=useState(0);
+//   const [bad,setBad]=useState(0);
+
+//   return (
+//     <div>
+//      <div>
+//      <h1>Give FeedBack</h1>
+//      <button onClick={()=> setGood(good + 1)}>Good</button>
+//      <button onClick={()=> setNeutral(neutral + 1)}>Neutral</button>
+//      <button onClick={()=> setBad(bad + 1)}>Bad</button>
+//      </div>
+
+//      <div>
+//       <h1>Statistics</h1>
+//       <p>Good : {good}</p>
+//       <p>Neutral : {neutral}</p>
+//       <p>Bad : {bad}</p>
+//       <p>All : {good + neutral +bad}</p>
+//       <p>Average : {(good+ neutral +bad)/3}</p>
+//       <p>Postitive : {(good)/(good+ neutral +bad)*100} %</p>
+//     </div>
+
+//    </div>
+//   )
+// }
+
+// export default App;
